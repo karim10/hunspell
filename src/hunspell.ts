b0c0111d2843fdafc079567d6b89b05b647b213d
Karim Ben Yezza <karim.b.yezza@gmail.com>
2021-01-03 21:15:42 +0100

    change default language to english

--- src/hunspell.ts
+++ src/hunspell.ts
@@ -1,7 +1,7 @@
 import { Nodehun } from 'nodehun';
 import fs from 'fs';
 
-const affix = fs.readFileSync('./dictionaries/fr_fr/fr_fr_med.aff');
-const dictionary = fs.readFileSync('./dictionaries/fr_fr/fr_fr_med.dic');
+const affix = fs.readFileSync('./dictionaries/en_us/en_us_med.aff');
+const dictionary = fs.readFileSync('./dictionaries/en_us/en_us_med.dic');
 
 export const nodehun = new Nodehun(affix, dictionary);
