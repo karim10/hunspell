a163796bd96aee5ead5a0203f6f2e56505056bf4
Karim Ben Yezza <karim.b.yezza@gmail.com>
2021-01-06 18:37:04 +0100

    add spellSync and spellAsync routes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,42 @@
 import express from 'express';
 import { nodehun } from './nodehun';
+import { SpellRequest, Word } from './types';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
     return res.status(200).send('Hunspell');
 });
+
+// app.use(function (_req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
+app.use(express.json({ limit: '50mb' }));
+
+
+app.post('/spellSync', (req: express.Request<SpellRequest>, res: express.Response) => {
+    const { locale, words } = req.body as SpellRequest;
+    const mispelledWords = words.filter(w => !nodehun.spellSync(w.str));
+    return res.json(mispelledWords);
+})
+
+app.post('/spellAsync', async (req: express.Request<SpellRequest>, res: express.Response) => {
+    const { locale, words } = req.body as SpellRequest;
+    const mispelledWords: Word[] = [];
+    words.forEach((w) => {
+        if (!nodehun.spell(w.str)) {
+            return mispelledWords.push(w);
+        }
+    })
+
+    return res.json(mispelledWords);
+})
 
 const port = process.env.port || 4000;
 
 app.listen(port, () => {
-    console.log('hello');
+    console.log(`Hunspell server listetning on port ${port}`);
 })
